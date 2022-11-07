3bf41ec437e14a8907c053e774383efbe176bbb8
Guilherme Kuhn <98616251+GDKuhn@users.noreply.github.com>
2022-11-07 16:11:54 -0300

    Updates

---
+++ precache-manifest.3662789264f28d4dbc2388d91171ad27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "266d0cff99c9ea9f4e39",
+    "url": "/jogo-da-memoria/static/css/main.68e2ecbb.chunk.css"
+  },
+  {
+    "revision": "266d0cff99c9ea9f4e39",
+    "url": "/jogo-da-memoria/static/js/main.266d0cff.chunk.js"
+  },
+  {
+    "revision": "bfa36157bcc75fb8ea07",
+    "url": "/jogo-da-memoria/static/js/1.bfa36157.chunk.js"
+  },
+  {
+    "revision": "15e80ec1ac795034716e",
+    "url": "/jogo-da-memoria/static/js/2.15e80ec1.chunk.js"
+  },
+  {
+    "revision": "34c791a81e14da573eff",
+    "url": "/jogo-da-memoria/static/js/runtime~main.34c791a8.js"
+  },
+  {
+    "revision": "6b72f2dce8ffb26889e6c2dd23c15c1c",
+    "url": "/jogo-da-memoria/index.html"
+  }
+];
